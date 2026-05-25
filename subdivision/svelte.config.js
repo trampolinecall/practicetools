f3fa193066595e443f16d103964c9fe5cd9e6344
trampolinecall <89745900+trampolinecall@users.noreply.github.com>
2026-05-25 00:03:58 -0400

    setup github pages to deploy but i dont know if it will work correctly

--- subdivision/svelte.config.js
+++ subdivision/svelte.config.js
@@ -2,17 +2,20 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			strict: true
-		})
-	},
+    compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+    },
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            strict: true
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : `${process.env.BASE_PATH}/subdivision`,
+        },
+    },
     output: {
         bundleStrategy: 'inline',
     },
